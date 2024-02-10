ede330cb73f329023905ffa3047fedabd8c90ef3
Harsh Mehrotra <harshmehrotra01@gmail.com>
2024-02-10 17:00:20 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Marquee() {
-  return (
-    <div className=''>
-MArquee
-    </div>
-  )
+	return (
+		<div className="z-[2] w-full py-16 rounded-2xl bg-[#004D43] ">
+			<div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden">
+				<h1 className='text-[17vw] leading-none font-["Test_Founders_Grotesk_X-Cond_SmBd"] -mb-12 uppercase'>
+					we are ochi!
+				</h1>
+				<h1 className='text-[17vw] leading-none font-["Test_Founders_Grotesk_X-Cond_SmBd"]-mb-12 uppercase'>
+					we are ochi!
+				</h1>
+			</div>
+		</div>
+	);
 }
 
-export default Marquee
+export default Marquee;
