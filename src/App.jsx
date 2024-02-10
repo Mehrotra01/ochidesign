01dda6f7936dc87277dfe84be60a4e40a8ef2c94
Harsh Mehrotra <harshmehrotra01@gmail.com>
2024-02-10 16:59:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@
 import Front from "./components/front";
 import Navbar from "./components/navbar";
 import Marquee from "./components/Marquee";
+import About from "./components/about";
 
 function App() {
-  return (
-    <>
-      <div className=" w-full min-h-screen bg-zinc-900 text-white">
-        <Navbar />
-        <Front />
-        <Marquee />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=" w-full min-h-screen bg-zinc-900 text-white">
+				<Navbar />
+				<Front />
+				<Marquee />
+				<About />
+			</div>
+		</>
+	);
 }
 
 export default App;
